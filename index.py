65550e7411a2adb2b9a8794ab8b61fa0c80b0830
divotlt <150188672+divotlt@users.noreply.github.com>
2026-03-23 09:24:56 -0700

    Update index.py

--- index.py
+++ index.py
@@ -30,8 +30,8 @@ bot_loop = None
 # ========================
 app = Flask(__name__)
 # Secure the session cookies
-app.secret_key = os.getenv("FLASK_SECRET", os.urandom(24))
-DASH_PASSWORD = os.getenv("DASH_PASSWORD") # Change this in your .env!
+app.secret_key = os.getenv("FLASK_SECRET", "secret123")
+DASH_PASSWORD = os.getenv("DASH_PASSWORD", "admin123")) # Change this in your .env!
 
 HTML_TEMPLATE = """
 <!DOCTYPE html>
